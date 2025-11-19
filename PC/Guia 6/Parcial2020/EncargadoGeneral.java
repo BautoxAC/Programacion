2798d4ef0cd8b259a20b6b7732cdf8ddbb8a863d
Bautista Tosi <tosibautista@gmail.com>
2025-11-19 11:17:12 -0300

    HEcho el parcial

--- PC/Guia 6/Parcial2020/EncargadoGeneral.java
+++ PC/Guia 6/Parcial2020/EncargadoGeneral.java
@@ -1,33 +1,36 @@
 import java.util.Random;
 
-public class EncargadoGeneral {
-     private Canasta[] canastas;
-    private int pizarra;
-    //Va a recibir tres canastas en el arreglo la primera sera la general, la segunda la contable y la tercera la de encargado
+public class EncargadoGeneral implements Runnable {
+    private Canasta[] canastas;
+    private int totalGlobal;
+
+    // Va a recibir tres canastas en el arreglo la primera sera la general, la
+    // segunda la contable y la tercera la de encargado
     public EncargadoGeneral(Canasta[] c) {
         this.canastas = c;
-        pizarra = 0;
+        totalGlobal = 0;
     }
+
     @Override
     public void run() {
         Random random = new Random();
         int turno = random.nextInt(1);
+        Ficha ficha = null;
         try {
-             if (turno == 0) {
-            Ficha ficha= canastas[0].sacarFicha();
-            System.out.println("Saque ficha y anontando");
-            Thread.sleep(3000);
-            pizarra+= ficha.importe;
-            canastas[2].ponerFicha(ficha);
-        } else {
-            Ficha ficha= canastas[1].sacarFicha();
-            System.out.println("Saque ficha y anontando");
-            Thread.sleep(3000);
-            pizarra+= ficha.importe;
-        }
+            if (turno == 0) {
+                ficha = canastas[0].sacarFicha();
+                System.out.println("Saque ficha y anontando");
+                Thread.sleep(3000);
+                canastas[1].ponerFicha(ficha);
+            } else {
+                ficha = canastas[2].sacarFicha();
+                System.out.println("Saque ficha y anontando");
+                Thread.sleep(3000);
+            }
+            totalGlobal += ficha.importe;
         } catch (Exception e) {
             // TODO: handle exception
         }
-       
+
     }
 }
